523e183628737bd20a393df54d268f46ebfa6a4f
SAPAAAA <hoanglanbui1967@gmail.com>
2025-03-29 14:54:38 +0700

    Implement created UIElement components

--- frontend/src/features/posts/components/PostPreview/PostPreview.jsx
+++ frontend/src/features/posts/components/PostPreview/PostPreview.jsx
@@ -1,34 +1,105 @@
 import React from "react";
 
+import Avatar from "@shared/components/UIElement/Avatar/Avatar";
+import Identifier from "@shared/components/UIElement/Identifier/Identifier";
+import Button from "@shared/components/UIElement/Button/Button";
+import Icon from "@shared/components/UIElement/Icon/Icon";
+
+import "./PostPreview.css";
+import {useVote} from "./vote-hook.js"
+
 export default function PostPreview(props) {
-    return (
-        <div className="card p-3 my-3">
-            {/* Subreddit Info */}
-            <div className="d-flex align-items-center mb-2">
-                <span className="text-muted">{props.post.subreddit} • {props.post.time}</span>
-            </div>
+	const {
+		voteStatus,
+		voteCount,
+		handleUpvote,
+		handleDownvote
+	} = useVote(props.post.upvotes);
 
-            {/* Post Title */}
-            <h5 className="fw-bold">{props.post.title}</h5>
+	return (
+		<div className="card p-3 my-3">
+			{/* Subreddit Info */}
+			<div className="d-flex align-items-center mb-2">
+				<Avatar
+					src={props.post.subtable.avatar.src}
+					alt={`r/${props.post.subtable.namespace}`}
+					width={20}
+					height={20}/>
+				<div className="d-flex flex-row flex-wrap fs-8">
+					<Identifier
+						addClass="ms-2"
+						type="subtable"
+						namespace={props.post.subtable.namespace}/>
+					&nbsp;•&nbsp;
+					<span className="text-muted">{props.post.time}</span>
+				</div>
+			</div>
 
-            {/* Post Content */}
-            <p>{props.post.content}</p>
+			{/* Post Title */}
+			<h5 className="fw-bold">{props.post.title}</h5>
 
-            {/* Post Actions */}
-            <div className="d-flex align-items-center">
-                <button className="btn btn-outline-light me-2">
-                    <i className="bi bi-arrow-up"></i> {props.post.upvotes}
-                </button>
-                <button className="btn btn-outline-light me-2">
-                    <i className="bi bi-arrow-down"></i>
-                </button>
-                <button className="btn btn-outline-light me-2">
-                    <i className="bi bi-chat-left"></i> {props.post.comments}
-                </button>
-                <button className="btn btn-outline-light">
-                    <i className="bi bi-share"></i> Share
-                </button>
-            </div>
-        </div>
-    );
+			{/* Post Content */}
+			<p>{props.post.content}</p>
+
+			{/* Post Actions */}
+			<div className="d-flex align-items-center gap-2">
+				<div
+					className={`vote-container ${voteStatus} d-flex align-items-center rounded-pill gap-2 bg-light`}>
+					<Button
+						mainClass="upvote-button"
+						contentType="icon"
+						tooltip="Upvote"
+						tooltipPlacement="top"
+						padding="2"
+						onClick={handleUpvote}
+					>
+						<Icon
+							mainClass="upvote-icon"
+							name={voteStatus === "upvoted" ? "upvoted" : "upvote"}
+							size="15px"/>
+					</Button>
+					<span className="fs-8">{voteCount}</span>
+					<Button
+						mainClass="downvote-button"
+						contentType="icon"
+						tooltip="Downvote"
+						tooltipPlacement="top"
+						padding="2"
+						onClick={handleDownvote}
+					>
+						<Icon
+							mainClass="downvote-icon"
+							name={voteStatus === "downvoted" ? "downvoted" : "downvote"}
+							size="15px"/>
+					</Button>
+				</div>
+				<div className="comment-container d-flex align-items-center rounded-pill gap-2 bg-light">
+					<Button
+						contentType="icon"
+						tooltip="Comment"
+						tooltipPlacement="top"
+						padding="2"
+					>
+						<Icon
+							mainClass="comment-icon"
+							name="comment"
+							size="15px"/>
+					</Button>
+				</div>
+				<div className="share-container d-flex align-items-center rounded-pill gap-2 bg-light">
+					<Button
+						contentType="icon"
+						tooltip="Share"
+						tooltipPlacement="top"
+						padding="2"
+					>
+						<Icon
+							mainClass="share-icon"
+							name="share"
+							size="15px"/>
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
