da4810768f6864391ab4a5a08e747ee94f9dd5e4
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-05 19:15:28 +0700

    Add isDropdown param

--- frontend/src/shared/components/UIElement/Link/Link.jsx
+++ frontend/src/shared/components/UIElement/Link/Link.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Link.css';
 
 export default function Link(props) {
-    const {href, children, className, ...rest} = props;
+    const {href, children, className = '', isDropdown, ...rest} = props;
+
+    const classes = `link text-decoration-none ${isDropdown !== undefined ? 'dropdown-item' : ''} ${className}`.trim();
 
     return (
         <a
             href={href}
-            className={`link ${className}`}
+            className={classes}
             {...rest}
         >
             {children}
