06585d5ee7623fab9659163a94f60c040e22a635
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-09 11:27:53 +0700

    Fix import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import * as ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import {AuthProvider} from "@contexts/AuthContext.jsx";
-import {SidebarProvider} from "@contexts/SidebarContext.jsx";
+import AuthProvider from "@contexts/AuthContext.jsx";
+import SidebarProvider from "@contexts/SidebarContext.jsx";
 import LoadingSpinner from "@shared/components/UIElement/LoadingSpinner/LoadingSpinner";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
