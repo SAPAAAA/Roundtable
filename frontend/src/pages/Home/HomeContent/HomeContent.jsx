f66038cab6e205b3f77ce88c29f569c3b3ba8639
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-09 09:41:52 +0700

    :truck: Refactor

--- frontend/src/pages/Home/HomeContent/HomeContent.jsx
+++ frontend/src/pages/Home/HomeContent/HomeContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import ListPostPreview from "@features/posts/components/ListPostPreview/ListPostPreview";
 
-export default function Home() {
+export default function HomeContent() {
     const posts = [
         {
             subtable: {
