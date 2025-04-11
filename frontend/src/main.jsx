60eb36b088a0d701709b39452a09f1a2f7123d56
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-11 21:36:19 +0700

    Remove unnecessary Suspense

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,18 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 import AuthProvider from "@contexts/AuthContext.jsx";
 import SidebarProvider from "@contexts/SidebarContext.jsx";
-import LoadingSpinner from "@shared/components/UIElement/LoadingSpinner/LoadingSpinner";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Suspense fallback={
-            <LoadingSpinner
-                message="Đang tải nội dung trang..."
-                overlayOpacity={0.01}
-                mainClass="page-loading"
-            />
-        }>
-            <AuthProvider>
-                <SidebarProvider>
-                    <App/>
-                </SidebarProvider>
-            </AuthProvider>
-        </Suspense>
+        <AuthProvider>
+            <SidebarProvider>
+                <App/>
+            </SidebarProvider>
+        </AuthProvider>
     </React.StrictMode>
 );
