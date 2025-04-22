5ae0961e32bf7a76a1c20ea7c932863105b92985
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-22 15:49:42 +0700

    Fix PostPreview

--- frontend/src/features/posts/components/ListPostPreview/ListPostPreview.jsx
+++ frontend/src/features/posts/components/ListPostPreview/ListPostPreview.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 
-import PostPreview from "#features/posts/components/PostPreview/PostPreview";
-
 export default function ListPostPreview(props) {
     return (
         <div className="list-post-preview">
-            {props.posts.map((post, index) => (
-                <PostPreview key={index} post={post}/>
-            ))}
         </div>
     );
 }
