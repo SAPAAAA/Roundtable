fe29cf50d5e55e52c9fd6590289a8dcb9be072aa
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-24 20:55:48 +0700

    Create Error classes

---
+++ backend/errors/AppError.js
@@ -0,0 +1,36 @@
+// src/errors/AppError.js
+class AppError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+class BadRequestError extends AppError {
+}
+
+class AuthenticationError extends AppError {
+} // For login failures, invalid tokens etc.
+class ForbiddenError extends AppError {
+} // For permission issues, inactive accounts etc.
+class NotFoundError extends AppError {
+}
+
+class ConflictError extends AppError {
+} // For existing username/email
+class VerificationError extends AppError {
+} // For invalid/expired codes
+class InternalServerError extends AppError {
+} // For unexpected issues
+
+export {
+    AppError,
+    BadRequestError,
+    AuthenticationError,
+    ForbiddenError,
+    NotFoundError,
+    ConflictError,
+    VerificationError,
+    InternalServerError,
+};
