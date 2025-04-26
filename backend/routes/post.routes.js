d4935c7e3f93c1f9f00f7fe90b523043d324e2ac
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-26 19:22:12 +0700

    Add a route for voting on post

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import postController from '#controllers/post.controller.js';
 import commentController from "#controllers/comment.controller.js";
+import {isAuthenticated} from "#middlewares/auth.mdw.js";
 
 const router = express.Router();
 
 router.get('/:postId', postController.getPostDetails);
-
-router.post('/:postId/comments', commentController.addComment);
+router.post('/:postId/comments', isAuthenticated, commentController.addComment);
+router.post('/:postId/vote', isAuthenticated, postController.castVote);
 
 export default router;
