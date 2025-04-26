f8cd7108d9744a245048e3308b66e657a1386790
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-26 15:51:09 +0700

    Rename ext and create 3 functions for create, update and delete actions

---
+++ frontend/src/services/voteService.jsx
@@ -0,0 +1,56 @@
+// src/services/PostService.js
+import {sendApiRequest} from "#utils/apiClient";
+
+class VoteService {
+    async createVote(postId, commentId, voteType) {
+        try {
+            let url;
+
+            if (commentId) {
+                url = `/api/comments/${commentId}/vote`;
+            } else {
+                url = `/api/posts/${postId}/vote`;
+            }
+
+            const response = await sendApiRequest(url, {
+                method: 'POST',
+                body: {
+                    voteType: voteType,
+                },
+            });
+
+            console.log("VoteService.createVote response:", response);
+            return response;
+        } catch (error) {
+            console.error("Error casting vote:", error);
+            throw error;
+        }
+    }
+
+    async deleteVote(voteId) {
+        try {
+            const url = `/api/votes/${voteId}`;
+            return await sendApiRequest(url, {method: 'DELETE'});
+        } catch (error) {
+            console.error("Error deleting vote:", error);
+            throw error;
+        }
+    }
+
+    async updateVote(voteId, voteType) {
+        try {
+            const url = `/api/votes/${voteId}`;
+            return await sendApiRequest(url, {
+                method: 'PATCH',
+                body: {
+                    voteType: voteType,
+                },
+            });
+        } catch (error) {
+            console.error("Error updating vote:", error);
+            throw error;
+        }
+    }
+}
+
+export default new VoteService();
