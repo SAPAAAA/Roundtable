49b0d8ad261641a5c60e0f8da174e1f7f543fd25
SAPAAAA <hoanglanbui1967@gmail.com>
2025-04-26 20:04:57 +0700

    Add vote and comment routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ import redis from '#db/redis.js'
 
 import authRoutes from '#routes/auth.routes.js'
 import postRoutes from "#routes/post.routes.js";
+import voteRoutes from "#routes/vote.routes.js";
+import commentRoutes from "#routes/comment.routes.js";
 
 dotenv.config()
 
@@ -62,6 +64,8 @@ app.use(
 
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/votes', voteRoutes);
+app.use('/api/comments', commentRoutes);
 
 
 app.listen(5000, () => {
